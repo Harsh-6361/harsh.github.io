ea163b00e841f0c798fb3b92b7abf598cd2a4f86
Harsh-6361 <121291397+Harsh-6361@users.noreply.github.com>
2024-06-09 15:42:26 +0530

    comment

--- footer.php
+++ footer.php
@@ -1,19 +1,48 @@
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
+<br></br><!-- This line is used to create a line break in the HTML document. -->
+
 <footer style="background-color: #f0f0f0; padding: 10px; text-align: center; clear: both;">
+  <!-- This is the footer section of the HTML document, which typically appears at the bottom of the page.
+  The 'style' attribute is used to apply CSS styles directly to the HTML element.
+  In this case, the footer has a light gray background color, padding of 10 pixels, centered text, and a clearance to avoid floating elements. -->
   <p>Copyright &copy; 2024 Project Members</p>
+  <!-- This paragraph tag contains the copyright information for the project members. -->
+
   <p>Get in touch with us:</p>
+  <!-- This paragraph tag contains a prompt for users to get in touch with the project members. -->
+
   <ul style="list-style: none; padding: 0; margin: 0;">
+    <!-- This unordered list (ul) element contains a list of project members with no bullet points.
+    The 'style' attribute is used to apply CSS styles directly to the HTML element.
+    In this case, the list has no padding or margin. -->
+
     <li>J HARSH VARDHAN (1SP22IC023)</li>
+    <!-- This list item (li) element contains the name and ID of the first project member. -->
+
     <li>GOKARNA R (1SP22IC017)</li>
+    <!-- This list item (li) element contains the name and ID of the second project member. -->
+
     <li>GAGAN S (1SP22IC018)</li>
+    <!-- This list item (li) element contains the name and ID of the third project member. -->
+
     <li>SANJAY (1SP22IC054)</li>
+    <!-- This list item (li) element contains the name and ID of the fourth project member. -->
+
     <li>VINAY KUMAR (1SP22IC060)</li>
+    <!-- This list item (li) element contains the name and ID of the fifth project member. -->
+
   </ul>
 </footer>
